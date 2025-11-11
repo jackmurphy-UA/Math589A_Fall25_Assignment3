33b3a7208ded9b7a6b967e83377eaa2a9c65650b
jackmurphy-UA <jackmurphy669@gmail.com>
2025-11-11 15:47:46 -0700

    Update qr_solve_dense.m

--- src/qr_solve_dense.m
+++ src/qr_solve_dense.m
@@ -1,7 +1,11 @@
 function x = qr_solve_dense(A,b)
 % QR_SOLVE_DENSE  Solve min ||A*x - b||_2 using explicit Householder QR.
 %   No backslash, pinv, chol, svd, regress, fitlm, arima, etc.
-    [m,n] = size(A); R = A; y = b;
+
+    [m,n] = size(A);
+    R = A;
+    y = b;
+
     for k = 1:n
         v = R(k:end,k);
         alpha = -sign(v(1)) * norm(v);
@@ -10,7 +14,10 @@ function x = qr_solve_dense(A,b)
         R(k:end,k:end) = R(k:end,k:end) - 2*v*(v.'*R(k:end,k:end));
         y(k:end)       = y(k:end)       - 2*v*(v.'*y(k:end));
     end
-    R = triu(R(1:n,1:n)); x = zeros(n,1);
+
+    R = triu(R(1:n,1:n));
+    x = zeros(n,1);
+
     for i = n:-1:1
         x(i) = (y(i) - R(i,i+1:end)*x(i+1:end)) / R(i,i);
     end
