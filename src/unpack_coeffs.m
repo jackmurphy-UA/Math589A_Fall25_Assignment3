2e53d162725d286378adc1955597376a02fb7ab3
jackmurphy-UA <jackmurphy669@gmail.com>
2025-11-11 15:57:07 -0700

    Update unpack_coeffs.m

--- src/unpack_coeffs.m
+++ src/unpack_coeffs.m
@@ -1,10 +1,7 @@
 function coef = unpack_coeffs(beta, N, K)
-% UNPACK_COEFFS  Map beta into named fields matching autograder expectation:
-% [c, alpha(1:K), beta(1:K), a(1:N)]
-
     coef.c     = beta(1);
     coef.alpha = beta(2:1+K);
     coef.beta  = beta(2+K:1+2*K);
     coef.a     = beta(2+2*K:1+2*K+N);
-    coef.d     = 0;  % dummy field required by autograder
+    coef.d     = 0;
 end
