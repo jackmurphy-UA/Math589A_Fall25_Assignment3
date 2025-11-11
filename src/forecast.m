d55b4bb91c3039cce99c32dacf4d62abf03cb7b3
jackmurphy-UA <jackmurphy669@gmail.com>
2025-11-11 15:48:02 -0700

    Update forecast.m

--- src/forecast.m
+++ src/forecast.m
@@ -1,7 +1,7 @@
 function yF = forecast(y, s, coef, H)
-% FORECAST  H-step-ahead forecast (no linear trend term).
+% FORECAST  H-step-ahead forecast (no trend term).
 
-    y = y(:); 
+    y = y(:);
     T = numel(y);
     N = numel(coef.a);
     K = numel(coef.alpha);
@@ -10,17 +10,11 @@ function yF = forecast(y, s, coef, H)
 
     for h = 1:H
         t = T + h;
-
-        % seasonal component
         sea = 0;
         for k = 1:K
-            sea = sea + coef.alpha(k)*cos(2*pi*k*t/s) ...
-                      + coef.beta(k) *sin(2*pi*k*t/s);
+            sea = sea + coef.alpha(k)*cos(2*pi*k*t/s) + coef.beta(k)*sin(2*pi*k*t/s);
         end
-
         acc = coef.c + sea;
-
-        % AR / difference part using available past (y or yF)
         for i = 1:N
             if h - i <= 0
                 acc = acc + coef.a(i)*y(T-(i-1));
@@ -28,7 +22,6 @@ function yF = forecast(y, s, coef, H)
                 acc = acc + coef.a(i)*yF(h-i);
             end
         end
-
-        yF(h) = acc;  % no trend term
+        yF(h) = acc;
     end
 end
