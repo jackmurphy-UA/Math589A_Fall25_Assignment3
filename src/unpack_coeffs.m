e5394488f55f93c582fd5366e1a9393f46e5be86
jackmurphy-dot <jackmurphy669@gmail.com>
2025-11-10 23:34:20 -0700

    Update unpack_coeffs.m

--- src/unpack_coeffs.m
+++ src/unpack_coeffs.m
@@ -1,6 +1,20 @@
 function coef = unpack_coeffs(beta, N, K)
+% UNPACK_COEFFS  Map beta into named fields for convenience.
+
+    coef = struct();
     coef.c = beta(1);
-    coef.a = beta(2:1+N);
-    coef.alpha = beta(1+N+1 : 1+N+K);
-    coef.beta  = beta(1+N+K+1 : 1+N+2*K);
+
+    if N > 0
+        coef.a = beta(2:1+N);
+    else
+        coef.a = zeros(0,1);
+    end
+
+    if K > 0
+        coef.alpha = beta(1+N+1 : 1+N+K);
+        coef.beta  = beta(1+N+K+1 : 1+N+2*K);
+    else
+        coef.alpha = zeros(0,1);
+        coef.beta  = zeros(0,1);
+    end
 end
