44c3b94e7c2a0593a9f2561ec76c1143964dc9c0
jackmurphy-dot <jackmurphy669@gmail.com>
2025-11-10 23:34:05 -0700

    Update select_model.m

--- src/select_model.m
+++ src/select_model.m
@@ -1,16 +1,23 @@
 function best = select_model(y, s, Ngrid, Kgrid, criterion)
-best = struct('score', Inf);
-for N = Ngrid(:).'
-  for K = Kgrid(:).'
-    try
-      fit = fit_once(y, s, N, K);
-    catch
-      continue
+% SELECT_MODEL  Grid-search over N and K with AIC/BIC scoring.
+
+    best = struct('score', Inf, 'N', NaN, 'K', NaN, 'coef', [], ...
+                  'RSS', [], 'M', [], 'p', [], 's', s, 'criterion', criterion);
+
+    for N = Ngrid(:).'
+        for K = Kgrid(:).'
+            try
+                fit = fit_once(y, s, N, K);
+            catch
+                % Under/ill-determined combos are skipped
+                continue
+            end
+            S = score_model(fit.RSS, fit.M, fit.p, criterion);
+            if S < best.score
+                best = fit;
+                best.score = S;
+                best.criterion = criterion;
+            end
+        end
     end
-    S = score_model(fit.RSS, fit.M, fit.p, criterion);
-    if S < best.score
-        best = fit; best.score = S; best.criterion = criterion;
-    end
-  end
-end
 end
