1429fadb353e7462aa3e126583d8dfa50e340b73
jackmurphy-dot <jackmurphy669@gmail.com>
2025-11-10 23:33:54 -0700

    Update score_model.m

--- src/score_model.m
+++ src/score_model.m
@@ -1,8 +1,16 @@
 function S = score_model(RSS, M, p, criterion)
-    if nargin<4 || isempty(criterion), criterion = 'bic'; end
-    if strcmpi(criterion,'bic')
-        S = M*log(RSS/M) + p*log(M);
-    else
-        S = M*log(RSS/M) + 2*p;
+% SCORE_MODEL  Information criterion score (lower is better).
+%   criterion: 'bic' (default) or 'aic'
+
+    if nargin < 4 || isempty(criterion), criterion = 'bic'; end
+
+    mse = RSS / M;  % unbiased scale in the log term
+    switch lower(criterion)
+        case 'bic'
+            S = M*log(mse) + p*log(M);
+        case 'aic'
+            S = M*log(mse) + 2*p;
+        otherwise
+            error('Unknown criterion: %s', criterion);
     end
 end
